ab148e01eb384b5a6f6f4fb937e102267c9cbedf
alexsaltstein <asaltste@stevens.edu>
2019-10-30 09:13:09 -0400

    Changed title animation

--- js/titleChanger.js
+++ js/titleChanger.js
@@ -1,19 +1,26 @@
-var counter = 1;
 const name = "Alex Saltstein";
-const possible = "-+*/|}{[]~:;?/.><=+-_)(*&^%$#@!)}";
+const possible = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 window.onload = function(){
     var looper = window.setInterval(function changeTitle(){
         var titleElem = document.getElementById("pageTitle");
         var randomChars = "";
         if (titleElem.innerHTML != name){
-            for (i = 0; i < (name.length - counter); i++){
-                randomChars += possible.charAt(Math.floor(Math.random() * possible.length));
+            var newName = titleElem.innerHTML;
+            var avail = "";
+            for(i = 0; i < newName.length; i++){
+                if(newName.charAt(i) != name.charAt(i)){
+                    avail += i + ",";
+                }
             }
-            const newName = name.substring(0,counter)+randomChars;
-            titleElem.innerHTML = newName;
-            counter++;
+            const availArr = avail.split(",");
+            var index = parseInt(availArr[(Math.floor(Math.random() * (availArr.length-1)))]);
+            newName = newName.substring(0,index) + name.charAt(index) + newName.substring(index+1, newName.length);
+            for (i = 0; i < name.length; i++){
+                randomChars += (newName.charAt(i) == name.charAt(i)) ? newName.charAt(i) : possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            titleElem.innerHTML = randomChars;
         }
 
-    }, 115)
+    }, 100)
 }
