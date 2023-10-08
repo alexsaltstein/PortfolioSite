a9ae920c0be5b3aa2068721b358239bda37b8107
Alex Saltstein <asaltste@gmail.com>
2023-10-07 20:13:58 -0400

    animations and roating working

--- js/test.js
+++ js/test.js
@@ -22,6 +22,20 @@ const removeScaleCursor = () => {
   cursor.classList.remove("scale-cursor");
 };
 
+const toggleRotate = (e) => {
+  const classList = e.classList;
+  const hasRotate = classList?.contains("rotate");
+  if (hasRotate === true) {
+    removeHover();
+    classList.add("moving");
+    classList.remove("rotate");
+  } else {
+    addHover();
+    classList.remove("moving");
+    classList.add("rotate");
+  }
+};
+
 const editCursor = (e) => {
   const cursor = document.getElementById("cursor");
   const { clientX: x, clientY: y } = e;
@@ -29,18 +43,33 @@ const editCursor = (e) => {
   cursor.style.top = y + "px";
 };
 
-const info = [{ text: "test" }, { text: "artichoke" }, { text: "banana" }];
+const info = [
+  { text: "test", back: "this is the back" },
+  { text: "artichoke", back: "another back" },
+  { text: "banana", back: "anotha one" },
+];
 
 window.onload = function () {
   window.addEventListener("mousemove", editCursor);
 
   const childs = [];
   for (let i = 0; i < info.length; i++) {
-    const d = document.createElement("div");
-    d.innerHTML = info[i].text;
+    const d = document.createElement("buton");
+    d.innerHTML = `
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+        ${info[i].text}
+        </div>
+        <div class="flip-card-back">
+        ${info[i].back}
+        </div>
+      </div>
+    `;
 
-    d.classList.add("box");
+    d.classList.add("flip-card");
     d.classList.add("hover-this");
+    d.classList.add("moving");
+
     const val = Math.random() * 20;
     d.style.setProperty("margin-top", `${val - 10}px`);
 
@@ -70,16 +99,17 @@ window.onload = function () {
     d.style.setProperty("background", opt.background);
     d.style.setProperty("color", opt.text);
     d.style.setProperty("animation-delay", `${Math.random() * 2}s`);
-
+    d.setAttribute("onclick", "toggleRotate(this)");
     childs.push(d);
   }
 
   const elems = document.getElementsByClassName("marquee");
   for (let i = 0; i < elems.length; i++) {
-    elems[i].addEventListener("mouseover", addHover);
-    elems[i].addEventListener("mouseout", removeHover);
+    // elems[i].addEventListener("mouseover", addHover);
+    // elems[i].addEventListener("mouseout", removeHover);
     childs.forEach((e) => {
-      elems[i].appendChild(e.cloneNode(true));
+      const l = e.cloneNode(true);
+      elems[i].appendChild(l);
     });
   }
 
